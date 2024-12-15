c589da28f7796188dda2c686a3ef14bab50ffbd1
hurtaditoo <al415507@uji.es>
2024-12-15 14:15:39 +0100

    hotfix: Create link for organization

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,7 +4,16 @@ function Footer() {
     return (
         <div className="p-2 text-center bg-black mt-4">
             <p className="text-white font-semibold mb-2">Where Basketball Meets Data</p>
-            <p className="text-white">IRON GUARDIANS ©, 2025 | Developed for IronHack Web Dev Bootcamp</p>
+            <p className="text-white">
+                <a 
+                    href="https://github.com/iron-guardians" 
+                    target="_blank" 
+                    className="hover:underline"
+                >
+                    IRON GUARDIANS
+                </a>  
+                {" "} ©, 2025 | Developed for IronHack Web Dev Bootcamp 
+            </p>       
         </div>
     );
 }
