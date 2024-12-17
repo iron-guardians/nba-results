c6b42816f73f04260aca91c4ad2e636ecd690ea7
Ignacio Molina <molinanacho33@gmail.com>
2024-12-17 19:16:26 +0100

    hotfix: improve homepage responsivity

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 
 function HomePage() {
     return (
-        <div className="pt-32">
+        <div className="pt-32 mx-auto grid grid-cols-1 md:grid-cols-2 gap-y-4 gap-x-2 max-w-screen-md">
             {gamesData.response.map((game) => (
-                <Link key={game.id} to={`/game/${game.id}`}>
+                <Link className="w-fit mx-auto" key={game.id} to={`/game/${game.id}`}>
                     <GameCard key={game.id} game={game}/>
                 </Link>         
             ))}
