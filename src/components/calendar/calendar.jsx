0caa0415ecc4c6f0c5621e6999f195677be17f79
hurtaditoo <al415507@uji.es>
2024-12-20 14:55:43 +0100

    hotfix: Arrows visible without moving the screen

--- src/components/calendar/calendar.jsx
+++ src/components/calendar/calendar.jsx
@@ -21,7 +21,7 @@ const WeeklyCalendar = ({ onDayClick }) => {
       days.push(
         <div
           key={i}
-          className={`flex flex-col items-center px-2 py-2 cursor-pointer ${
+          className={`flex flex-col items-center px-1 py-2 cursor-pointer ${
             day.isSame(selectedDate, 'day') ? 'font-extrabold text-blue-500' : 'text-gray-500'
           }`}
           onClick={() => {
@@ -39,27 +39,29 @@ const WeeklyCalendar = ({ onDayClick }) => {
 
 
   return (
-    <div className="flex items-center justify-between p-4 bg-white shadow-md rounded-lg mb-4">
-      {/* Left Arrow */}
-      <button
-        onClick={() => changeWeek(-1)}
-        className="text-blue-500 text-xl font-bold hover:text-blue-700"
-      >
-        &#8249;
-      </button>
+    <div className="max-w-full overflow-x-hidden">
+        <div className="flex items-center justify-between p-4 bg-white mb-4">
+        {/* Left Arrow */}
+        <button
+            onClick={() => changeWeek(-1)}
+            className="text-blue-500 text-xl font-bold hover:text-blue-700"
+        >
+            &#8249;
+        </button>
 
-      {/* Days of the Week */}
-      <div className="flex justify-around flex-1 space-x-4 font-bold ">
-        {renderDaysOfWeek()}
-      </div>
+        {/* Days of the Week */}
+        <div className="flex justify-around flex-1 space-x-1 font-bold ">
+            {renderDaysOfWeek()}
+        </div>
 
-      {/* Right Arrow */}
-      <button
-        onClick={() => changeWeek(1)}
-        className="text-blue-500 text-xl font-bold hover:text-blue-700"
-      >
-        &#8250;
-      </button>
+        {/* Right Arrow */}
+        <button
+            onClick={() => changeWeek(1)}
+            className="text-blue-500 text-xl font-bold hover:text-blue-700"
+        >
+            &#8250;
+        </button>
+        </div>
     </div>
   );
 };
