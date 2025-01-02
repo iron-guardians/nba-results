31313346120a92c20f0117aa20ca87f444acb5dd
Ignacio Molina <molinanacho33@gmail.com>
2025-01-02 21:47:26 +0100

    Add conference selector

--- src/pages/standings-page.jsx
+++ src/pages/standings-page.jsx
@@ -7,6 +7,7 @@ function StandingsPage() {
   // Combine standings and team data
   const [eastRankedTeams, setEastRankedTeams] = useState([]);
   const [westRankedTeams, setWestRankedTeams] = useState([]);
+  const [selectedConference, setSelectedConference] = useState("east"); // State for selected conference
 
   useEffect(() => {
     DunkNationApi.getStandings().then((response) => {
@@ -39,24 +40,45 @@ function StandingsPage() {
 
   return (
     <div className="bg-gray-900 text-white min-h-screen pt-40">
-      <div className="container mx-auto pt-6 pb-12">
-        <h1 className="text-3xl font-semibold text-blue-400 mb-10 text-center">
-          East Conference
-        </h1>
-        <div className="grid grid-cols-1gap-y-8 gap-x-4">
-          <StandingContainer rankedTeams={eastRankedTeams} />
+    <div className="container mx-auto pt-6 pb-12">
+    <div className="text-center mb-6">
+          <button
+            onClick={() => setSelectedConference("east")}
+            className={`py-2 px-4 rounded-l-full border-r-0 ${selectedConference === "east" ? "bg-blue-500" : "bg-gray-700"} text-white`}
+          >
+            East Conference
+          </button>
+          <button
+            onClick={() => setSelectedConference("west")}
+            className={`py-2 px-4 rounded-r-full border-l-0 ${selectedConference === "west" ? "bg-blue-500" : "bg-gray-700"} text-white`}
+          >
+            West Conference
+          </button>
         </div>
-      </div>
 
-      <div className="container mx-auto pt-6 pb-12">
-        <h1 className="text-3xl font-semibold text-blue-400 mb-10 text-center">
-          West Conference
-        </h1>
-        <div className="grid grid-cols-1gap-y-8 gap-x-4">
-          <StandingContainer rankedTeams={westRankedTeams} />
-        </div>
-      </div>
+      {selectedConference === "east" && (
+        <>
+          <h1 className="text-3xl font-semibold text-blue-400 mb-10 text-center">
+            East Conference
+          </h1>
+          <div className="grid grid-cols-1 gap-y-8 gap-x-4">
+            <StandingContainer rankedTeams={eastRankedTeams} />
+          </div>
+        </>
+      )}
+
+      {selectedConference === "west" && (
+        <>
+          <h1 className="text-3xl font-semibold text-blue-400 mb-10 text-center">
+            West Conference
+          </h1>
+          <div className="grid grid-cols-1 gap-y-8 gap-x-4">
+            <StandingContainer rankedTeams={westRankedTeams} />
+          </div>
+        </>
+      )}
     </div>
+  </div>
   );
 }
 
