e676dcfc91053ba67c60d3e7b1c150768f42fb2c
hurtaditoo <al415507@uji.es>
2025-01-11 11:25:37 +0100

    task: Add more stats

--- src/components/stat-comparer-container/stat-comparer-container.jsx
+++ src/components/stat-comparer-container/stat-comparer-container.jsx
@@ -5,7 +5,7 @@ const StatComparerContainer = ({ children, className }) => {
   return (
     <div className={`rounded-lg shadow-lg w-full ${className}`}>
       <h2 className="text-3xl font-semibold text-blue-400 mb-10 text-center">
-        Stats Comparer
+        Stats Comparison
       </h2>
       <div
         className="grid grid-cols-1 md:grid-cols-2 gap-y-8 gap-x-4 shadow-inner grid place-items-center w-full"
